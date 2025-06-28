89fa67902b13b66f424dfe7d634a41a5967305e6
HuziiDev <huzaifaasif842@gmail.com>
2025-06-28 11:42:54 +0530

    first

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+   <div className="flex items-center justify-center ">
+    <Button >
+        Click Me
+    </Button>
+   </div>
+  );
+}
